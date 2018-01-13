da84dc622abda30316cd25387b7c5e846bf04e73
sujittripathy <sujit.tripathy@icloud.com>
2018-01-13 10:49:02 -0800

    A sample Spring Boot project to test CI

---
+++ src/main/java/com/boot/sample/SpringBootSample/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.boot.sample.SpringBootSample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/api/sample")
+	public String sampleAPI(){
+		return "Sample Response";
+	}
+}
